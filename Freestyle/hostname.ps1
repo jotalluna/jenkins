b17108402f8e187cce73377c694246763ec9f0e6
jotalluna <jotalluna@gmail.com>
2023-05-30 19:56:04 +0200

    Primer ejempl

---
+++ Freestyle/hostname.ps1
@@ -0,0 +1,2 @@
+Write-Host "Hola Mundo"
+hostname
---
+++ Freestyle/hostname.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Obtener hostname y guardarlo en una variable
+node_hostname=$(hostname)
+
+#Imprimir hostname
+echo $node_hostname
